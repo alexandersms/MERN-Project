0ff2e33147c70db405cf69537b226f2ac5c29315
Alex Simisi <smsxander@gmail.com>
2019-10-05 00:19:07 +0200

    features: Création de l'action et du reducers de connexion

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
-const rootReducer = combineReducers({
+import AuthentificationReducer from "./authentification";
 
+const rootReducer = combineReducers({
+  authentification: AuthentificationReducer
 });
 
 export default rootReducer;
